47b4443c3145eac34a53457c115dec1a24d09987
chenyanpeng <chenyanpeng@yaxon.com>
2024-11-14 13:59:14 +0800

    update

--- uds_service_31.c
+++ uds_service_31.c
@@ -2,9 +2,154 @@
 #include "uds_stream.h"
 #include <json-c/json.h>
 
+typedef struct routine_handler {
+	uint16_t rid;
+	uint8_t type;
+	int (*fp)(uds_context_t *uds_context, uint8_t control_type);
+} routine_handler_t;
+
+static int routine_handler_fe01_01(uds_context_t *uds_context, uint8_t control_type)
+{
+	uds_stream_t strm = {0};
+	uds_response_t *uds_response = &uds_context->uds_response;
+
+	uds_stream_init(&strm, uds_response->pos, uds_response->cap);
+	uds_stream_write_byte(&strm, 0x00);
+	return NRC_PositiveRespon_00;
+}
+
+static int routine_handler_fe01_03(uds_context_t *uds_context, uint8_t control_type)
+{
+	uds_stream_t strm = {0};
+	uds_response_t *uds_response = &uds_context->uds_response;
+
+	uds_stream_init(&strm, uds_response->pos, uds_response->cap);
+	uds_stream_write_byte(&strm, 0x00);
+	return NRC_PositiveRespon_00;
+}
+
+static routine_handler_t __gs_routine_handler_table__[] = {
+	{0xfe01, 0x01, routine_handler_fe01_01},
+	{0xfe01, 0x03, routine_handler_fe01_03},
+};
+
 int uds_service_31_handler(struct uds_context *uds_context, unsigned char *uds, int len)
 {
-	return 0;
+	uint16_t rid;
+	uint8_t sid, control_type;
+	uds_stream_t strm = {0};
+	uint8_t nrc = NRC_PositiveRespon_00;
+	uds_service_31_routine_t *routine = NULL;
+	uds_service_31_t *uds_service_31 = &uds_context->uds_service_31;
+
+	if (len < 4) {
+		nrc = NRC_IncorrectMessageLengthOrInvalidFormat_13;
+		goto finish;
+	}
+
+	uds_stream_init(&strm, uds, len);
+	sid = uds_stream_read_byte(&strm);
+	control_type = uds_stream_read_byte(&strm);
+	rid = uds_stream_read_be16(&strm);
+	for (int i = 0; i < uds_service_31->count; i++) {
+		if (uds_service_31->routines[i].rid == rid) {
+			routine = &uds_service_31->routines[i];
+			break;
+		}
+	}
+
+	/* RID不支持 */
+	if (!routine) {
+		nrc = NRC_RequestOutOfRange_31;
+		goto finish;
+	}
+
+	size_t idx = 0;
+	for (; idx < ARRAYSIZE(routine->control_type); idx++) {
+		if (routine->control_type[idx].type == control_type) {
+			break;
+		}
+	}
+
+	/* control_type不支持 */
+	if (idx == ARRAYSIZE(routine->control_type)) {
+		nrc = NRC_RequestOutOfRange_31;
+		goto finish;
+	}
+
+	/* 长度不合法 */
+	if ((int)routine->control_type[idx].len != (len - 4)) {
+		nrc = NRC_IncorrectMessageLengthOrInvalidFormat_13;
+		goto finish;
+	}
+
+	uint8_t pass = FALSE;
+	for (size_t i = 0; i < ARRAYSIZE(routine->control_type[idx].sessions); i++) {
+		if (uds_diagnostic_session(uds_context) == routine->control_type[idx].sessions[i]) {
+			pass = TRUE;
+			break;
+		}
+	}
+	/* 当前会话模式下不支持 */
+	if (!pass) {
+		nrc = routine->control_type[idx].session_nrc;
+		goto finish;
+	}
+
+	pass = FALSE;
+	for (size_t i = 0; i < ARRAYSIZE(routine->control_type[idx].security_access_levels); i++) {
+		if (uds_diagnostic_session(uds_context) == routine->control_type[idx].security_access_levels[i]) {
+			pass = TRUE;
+			break;
+		}
+	}
+	/* 当前安全级别下不支持 */
+	if (!pass) {
+		nrc = routine->control_type[idx].security_access_nrc;
+		goto finish;
+	}
+
+	for (size_t i = 0; i < ARRAYSIZE(__gs_routine_handler_table__); i++) {
+		if (__gs_routine_handler_table__[i].rid == routine->rid && __gs_routine_handler_table__[i].type == control_type) {
+			if (__gs_routine_handler_table__[i].fp) {
+				nrc = __gs_routine_handler_table__[i].fp(uds_context, control_type);
+			}
+			else {
+				nrc = NRC_ConditionsNotCorrect_22;
+			}
+			break;
+		}
+	}
+
+finish:
+	uds_context->nrc = nrc;
+	return nrc;
+}
+
+static void uds_service_31_routine_dump(uds_context_t *uds_context)
+{
+	char buffer[64] = {0};
+	uds_service_31_t *uds_service_31 = &uds_context->uds_service_31;
+
+	for (int i = 0; i < uds_service_31->count; i++) {
+		logd("RID:0x%x\n", uds_service_31->routines[i].rid);
+		for (size_t j = 0; j < ARRAYSIZE(uds_service_31->routines[i].control_type); j++) {
+			logd("desc:%s\n", uds_service_31->routines[i].control_type[j].desc);
+			logd("type:%d\n", uds_service_31->routines[i].control_type[j].type);
+			logd("len:%d\n", uds_service_31->routines[i].control_type[j].len);
+			for (size_t k = 0; k < ARRAYSIZE(uds_service_31->routines[i].control_type[j].sessions); k++) {
+				snprintf(buffer, ARRAYSIZE(buffer), "%d ", uds_service_31->routines[i].control_type[j].sessions[k]);
+			}
+			logd("sessions:[ %s]\n", buffer);
+			for (size_t k = 0; k < ARRAYSIZE(uds_service_31->routines[i].control_type[j].security_access_levels); k++) {
+				snprintf(buffer, ARRAYSIZE(buffer), "%d ", uds_service_31->routines[i].control_type[j].security_access_levels[k]);
+			}
+			logd("security_access_levels:[ %s]\n", buffer);
+			logd("session_nrc:0x%02x\n", uds_service_31->routines[i].control_type[j].session_nrc);
+			logd("security_access_nrc:0x%02x\n", uds_service_31->routines[i].control_type[j].security_access_nrc);
+		}
+		logd("-------------------------------------------------------------------\n");
+	}
 }
 
 static void uds_service_31_routine_parse(uds_context_t *uds_context, const char *filename)
@@ -41,16 +186,24 @@ static void uds_service_31_routine_parse(uds_context_t *uds_context, const char 
 			struct json_object *type_obj = json_object_object_get(control_type_item, "type");
 			struct json_object *len_obj = json_object_object_get(control_type_item, "len");
 			struct json_object *desc_obj = json_object_object_get(control_type_item, "desc");
+			struct json_object *session_nrc_obj = json_object_object_get(control_type_item, "session_nrc");
+			struct json_object *security_access_nrc_obj = json_object_object_get(control_type_item, "security_access_nrc");
 			struct json_object *sessions_obj = json_object_object_get(control_type_item, "sessions");
 			struct json_object *security_access_levels_obj = json_object_object_get(control_type_item, "security_access_levels");
 			uds_assert(json_object_is_type(type_obj, json_type_int), "type is not int");
 			uds_assert(json_object_is_type(len_obj, json_type_int), "len is not int");
 			uds_assert(json_object_is_type(desc_obj, json_type_string), "desc is not string");
+			uds_assert(json_object_is_type(session_nrc_obj, json_type_string), "session_nrc is not string");
+			uds_assert(json_object_is_type(security_access_nrc_obj, json_type_string), "security_access_nrc is not string");
 			uds_assert(json_object_is_type(sessions_obj, json_type_array), "sessions is not array");
 			uds_assert(json_object_is_type(security_access_levels_obj, json_type_array), "security_access_levels is not array");
 
 			uds_context->uds_service_31.routines[i].control_type[k].type = json_object_get_int(type_obj);
 			uds_context->uds_service_31.routines[i].control_type[k].len = json_object_get_int(len_obj);
+			uds_context->uds_service_31.routines[i].control_type[k].session_nrc =
+				strtoul(json_object_get_string(session_nrc_obj), NULL, 0x10);
+			uds_context->uds_service_31.routines[i].control_type[k].security_access_nrc =
+				strtoul(json_object_get_string(security_access_nrc_obj), NULL, 0x10);
 			memcpy(uds_context->uds_service_31.routines[i].control_type[k].desc, json_object_get_string(desc_obj), \
 					MIN((int)ARRAYSIZE(uds_context->uds_service_31.routines[i].control_type[k].desc), \
 						json_object_get_string_len(desc_obj)));
@@ -69,7 +222,7 @@ static void uds_service_31_routine_parse(uds_context_t *uds_context, const char 
 			}
 		}
 	}
-
+	uds_service_31_routine_dump(uds_context);
 	json_object_put(obj);
 }
 
--- uds_service_31.h
+++ uds_service_31.h
@@ -6,11 +6,13 @@ struct uds_context;
 typedef struct uds_service_31_routine {
 	unsigned short rid;
 	struct {
-		unsigned char type;
-		unsigned int len;
-		char desc[32];
-		unsigned char sessions[3];
-		unsigned char security_access_levels[3];
+		char desc[32]; /* RID描述 */
+		unsigned int len; /* 31服务参数长度, 不汗SID(1) + type(1) + RID(2) */
+		unsigned char type; /* 01 - 执行, 03 - 读取结果 */
+		unsigned char sessions[3]; /* 在该会话模式下可执行routine */
+		unsigned char security_access_levels[3]; /* 在该安全级别下可执行routine */
+		unsigned char session_nrc; /* 会话模式不通过返回该nrc */
+		unsigned char security_access_nrc; /* 安全级别不通过返回该nrc */
 	} control_type[2];
 } uds_service_31_routine_t;
 
